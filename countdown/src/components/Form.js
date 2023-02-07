252216e7d6f561ea50069f4b8b098baf18b87760
Kevin Ng <kevinwng23@gmail.com>
2023-02-07 17:21:53 -0500

    event name field

--- countdown/src/components/Form.js
+++ countdown/src/components/Form.js
@@ -1,6 +1,10 @@
-
-
-export default function Form (props) {
-    return (<h1>Hello Darion</h1>
-    )
+export default function Form(props) {
+  return (
+    <form>
+      <label>
+        <span>Name</span>
+        <input type="text" required name="name" />
+      </label>
+    </form>
+  );
 }
