31a6c8b0a5bc465954084a7fb70f5eaab947bf51
Kevin Ng <kevinwng23@gmail.com>
2023-02-07 17:36:19 -0500

    setting up usestate

--- countdown/src/components/Form.js
+++ countdown/src/components/Form.js
@@ -1,9 +1,26 @@
+import {useState, useEffect} from "react"
+
 export default function Form(props) {
+
+    const initForm = {
+        name: "",
+        date: "",
+        time: "",
+    }
+
+    const [newEvent, setNewEvent] = useState(initForm);
+
+
   return (
     <form>
       <label>
         <span>Name</span>
         <input type="text" required name="name" />
+
+        <span>Date</span>
+        <input type="text" required name="date" />
+
+
       </label>
     </form>
   );
