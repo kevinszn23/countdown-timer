87833c9b9d3e3f51d6f4cddf89c87f60a6fa6cb2
DarionDaire <DarionDaire@gmail.com>
2023-02-08 17:55:03 -0500

    Create Timer

--- countdown/src/App.js
+++ countdown/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Form from './components/Form';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Form />
+      <Timer />
+
     </div>
   );
 }
--- countdown/src/components/Form.js
+++ countdown/src/components/Form.js
@@ -1,27 +1,36 @@
 import {useState, useEffect} from "react"
+import Timer from "./Timer";
 
 export default function Form(props) {
 
-    const initForm = {
-        name: "",
-        date: "",
-        time: "",
-    }
+    // const initForm = {
+    //     name: "",
+    //     date: "",
+    //     time: "",
+    // }
 
-    const [newEvent, setNewEvent] = useState(initForm);
+    // const [newEvent, setNewEvent] = useState(initForm);
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('');
 
 
   return (
-    <form>
+    <div>
+    <form onSubmit={()=> {setDate(date)}}>
       <label>
         <span>Name</span>
-        <input type="text" required name="name" />
+        <input type="text" required name="name" value={name} onChange={(e) => {setName(e.target.value)} } />
 
         <span>Date</span>
-        <input type="text" required name="date" />
+        <input type="text" required name="date" value={date} onChange={(e) => {setDate(e.target.value)} } />
 
 
       </label>
+      <input type="submit" value="submit"/>
     </form>
+    {/* <Timer date={date}/> */}
+    </div>
+
+    
   );
 }
