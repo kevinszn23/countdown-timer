4cfeb3088ec2c15ee42d93e073f43fdecde5612b
Kevin Ng <kevinwng23@gmail.com>
2023-02-09 16:40:53 -0500

    fixing timer

--- countdown/src/components/Form.js
+++ countdown/src/components/Form.js
@@ -1,36 +1,85 @@
-import {useState, useEffect} from "react"
-import Timer from "./Timer";
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Form(props) {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [inputDate, setInputDate] = useState(new Date());
 
-    const initForm = {
-        name: "",
-        date: "",
-        time: "",
-    }
+  useEffect(() => {
+    startTimer();
+  }, []);
 
-    // const [newEvent, setNewEvent] = useState(initForm);
-    const [name, setName] = useState('')
-    const [date, setDate] = useState('');
+  let interval;
 
+  const startTimer = () => {
+    const countDownDate = new Date("February 14,2023").getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const count = countDownDate - now;
+
+      const timerDays = Math.floor(count / (24 * 60 * 60 * 1000));
+      const timerHours = Math.floor(
+        (count % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+      const timerMinutes = Math.floor((count % (60 * 60 * 1000)) / (1000 * 60));
+      const timerSeconds = Math.floor((count % (60 * 1000)) / 1000);
+
+      if (count < 0) {
+        clearInterval(interval);
+      } else {
+        setDays(timerDays);
+        setHours(timerHours);
+        setMinutes(timerMinutes);
+        setSeconds(timerSeconds);
+      }
+    });
+  };
 
   return (
     <div>
-    <form onSubmit={()=> {setDate(date)}}>
-      <label>
-        <span>Name</span>
-        <input type="text" required name="name" value={name} onChange={(e) => {setName(e.target.value)} } />
+      <form
+        onSubmit={() => {
+          setDate(date);
+        }}
+      >
+        <label>
+          <span>Name</span>
+          <input
+            type="text"
+            required
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
 
-        <span>Date</span>
-        <input type="text" required name="date" value={date} onChange={(e) => {setDate(e.target.value)} } />
+          <span>Date</span>
+          <input
+            type="text"
+            required
+            name="date"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="submit" />
 
-
-      </label>
-      <input type="submit" value="submit"/>
-    </form>
-    {/* <Timer date={date}/> */}
+        <div>
+          <p>{days} days</p>
+          <p>{hours} hours</p>
+          <p>{minutes} minutes</p>
+          <p>{seconds} seconds</p>
+        </div>
+      </form>
     </div>
-
-    
   );
 }
